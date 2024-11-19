feec4c0017790ce1955f85a138ee23e1c6b3386f
joaopma <joaopaulo.m@terra.com.br>
2024-11-18 21:09:34 -0300

    initial

---
+++ ISP/BAD_PRATICE/Employee.cs
@@ -0,0 +1,40 @@
+﻿namespace SOLID.ISP.BAD_PRATICE
+{
+    public interface IEmployee
+    {
+        string Name { get; set; }
+        void CalculateSalary();
+        void CalculateBenefits();
+    }
+
+    public class FullTimeEmployee : IEmployee
+    {
+        public string Name { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public void CalculateBenefits()
+        {
+            Console.WriteLine("Calculating benefits for full-time employee");
+        }
+
+        public void CalculateSalary()
+        {
+            Console.WriteLine("Calculating salary for full-time employee");
+        }
+    }
+
+    public class ContractEmployee : IEmployee
+    {
+        string IEmployee.Name { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        void IEmployee.CalculateBenefits()
+        {
+            //Erro aqui, empregado contratado não tem benefícios(por exemplo ele é PJ), então não faz sentido calcular os benefícios
+            throw new NotImplementedException();
+        }
+
+        void IEmployee.CalculateSalary()
+        {
+            Console.WriteLine("Calculating salary for contract employee");            
+        }
+    }
+}
---
+++ ISP/GOOD_PRATICE/Employee.cs
@@ -0,0 +1,33 @@
+﻿namespace SOLID.ISP.GOOD_PRATICE
+{
+    public interface ISalaryCalculator
+    {
+        void CalculateSalary();
+    }
+
+    public interface IBenefitsCalculator
+    {
+        void CalculateBenefits();
+    }
+
+    public class FullTimeEmployee : ISalaryCalculator, IBenefitsCalculator
+    {
+        public void CalculateBenefits()
+        {
+            Console.WriteLine("Calculating benefits for full-time employee");
+        }
+
+        public void CalculateSalary()
+        {
+            Console.WriteLine("Calculating salary for full-time employee");
+        }
+    }
+
+    public class ContractEmployee : ISalaryCalculator
+    {
+        public void CalculateSalary()
+        {
+            Console.WriteLine("Calculating salary for contract employee");
+        }
+    }
+}
