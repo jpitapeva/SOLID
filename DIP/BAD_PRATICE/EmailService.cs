77191dda1636d697ee6de4c5afaa660ff8b53310
joaopma <joaopaulo.m@terra.com.br>
2024-11-22 11:28:03 -0300

    Add DIP

---
+++ DIP/BAD_PRATICE/EmailService.cs
@@ -0,0 +1,19 @@
+﻿namespace SOLID.DIP.BAD_PRATICE
+{
+    public class EmailService
+    {
+        public void SendEmail(string email, string subject, string message)
+        {
+            //Send email
+        }
+    }
+
+    public class UserSerivce(EmailService emailService)
+    {
+        public void AddUser(string email)
+        {
+            //Add user
+            emailService.SendEmail(email, "Welcome", "Welcome to our system");
+        }
+    }
+}
---
+++ DIP/GOOD_PRATICE/EmailService.cs
@@ -0,0 +1,30 @@
+﻿namespace SOLID.DIP.GOOD_PRATICE
+{
+    public interface IEmailService
+    {
+        void SendEmail(string email, string subject, string message);
+    }
+    public class EmailService : IEmailService
+    {
+        public void SendEmail(string email, string subject, string message)
+        {
+            //Send email
+        }
+    }
+
+    public class EmailServiceFake : IEmailService
+    {
+        public void SendEmail(string email, string subject, string message)
+        {
+            //Send email
+        }
+    }
+
+    public class UserService : IEmailService
+    {
+        public void SendEmail(string email, string subject, string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
